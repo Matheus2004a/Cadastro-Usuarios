10bb77fcac82c8e42582bfa0399cd790f15f433a
Matheus Aurélio <matheusaurelio2004@gmail.com>
2021-09-28 19:48:59 -0300

    Adicionando arquivo pesquisa.css para página de pesquisa de dados

--- pesquisa.php
+++ pesquisa.php
@@ -7,6 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tabelas dos dados pessoais</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+    <link rel="stylesheet" href="pesquisa.css">
 </head>
 
 <body>
@@ -47,7 +48,7 @@
             <div class="container-fluid">
                 <form class="d-flex">
                     <input class="form-control me-2" type="search" placeholder="Pesquisar" aria-label="Search" name="busca-dados">
-                    <button class="btn btn-outline-success" type="submit">Pesquisar</button>
+                    <button class="btn btn-outline-success" type="submit">Procurar</button>
                 </form>
             </div>
         </nav>
@@ -76,7 +77,8 @@
                 </tr>
                 <tr>
                     <th scope="row">3</th>
-                    <td colspan="2">Larry the Bird</td>
+                    <td>Larry the Bird</td>
+                    <td>William Steve</td>
                     <td>@twitter</td>
                 </tr>
             </tbody>
