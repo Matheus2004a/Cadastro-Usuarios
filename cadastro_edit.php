2de3fcce2e1e762421cd7a157ff878038b1ebad0
Matheus Aurélio <matheusaurelio2004@gmail.com>
2021-10-18 17:24:44 -0300

    Adicionando no banco coluna de id dos usuários

--- cadastro_edit.php
+++ cadastro_edit.php
@@ -9,22 +9,16 @@ session_start();
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Alterações de cadastro no MySQL com PHP</title>
+    <title>Alterações de cadastro</title>
     <link rel="stylesheet" href="./home/forms.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
 </head>
 
 <body>
     <?php
         include_once("./connection/conexao.php");
-
-        if (isset($nome)) {
-            $nome = $_GET['nomeCompleto'];
-        } 
-        else {
-            "";
-        }
+        
+        $nome = $_GET['nomeCompleto'] ?? "";
 
         // Fazendo uma nova requisição no bd
         $sql = "SELECT * FROM tbl_dadosuser WHERE nome_completo = $nome";
@@ -90,6 +84,7 @@ session_start();
     </form>
 
     <script src="https://kit.fontawesome.com/798bcbaf05.js" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
 </body>
 
 </html>
